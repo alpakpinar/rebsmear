190eafa3fe3356a92669d59291c70b2cc6396bef
Andreas Albert <andreas.albert@cern.ch>
2021-01-21 13:05:51 +0100

    Implement pt-based fit

--- rebalance.py
+++ rebalance.py
@@ -81,7 +81,6 @@ class HistoSF2D():
         x = self._apply_limit(x, self._xmin, self._xmax)
         y = self._apply_limit(y, self._ymin, self._ymax)
 
-        print(self._xmin, self._xmax, x)
         bin_id = self._histogram.FindBin(x,y)
         return self._histogram.GetBinContent(bin_id)
 
@@ -121,9 +120,8 @@ class RebalanceWSFactory(NamingMixin):
         self.njets = len(jets)
         self.ws = r.RooWorkspace()
         self._wsimp = getattr(self.ws, 'import')
-        self._directions = 'px', 'py'
         self._jer_evaluator = None
-
+        self._directions = 'pt','phi'
     def set_jer_source(self,filepath, histogram_name):
         self._jer_evaluator = JERLookup(filepath, histogram_name)
 
@@ -191,9 +189,96 @@ class RebalanceWSFactory(NamingMixin):
         self._wsimp(likelihood)
 
     def _build_gen_htmiss_variables(self):
+        if self._directions==('pt','phi'):
+            self._build_gen_htmiss_xy_variable_from_pt_phi()
+        else:
+            self._build_gen_htmiss_xy_variable_from_px_py()
+        self._build_derived_gen_htmiss_pt_phi_variables()
+
+    def _build_gen_htmiss_xy_variable_from_pt_phi(self):
+        '''
+        Creates x and y variables for htmiss based on jet pt, phi inputs.
+        '''
+        expression_parts_x, expression_parts_y = [], []
+        variables = []
+        for index in range(self.njets):
+            var_name_pt = self._name_gen_momentum_var('pt', index)
+            var_name_phi = self._name_gen_momentum_var('phi', index)
+            variables.append(self.ws.var(var_name_phi))
+            variables.append(self.ws.var(var_name_pt))
+            expression_parts_x.append(f'{var_name_pt} * cos({var_name_phi})')
+            expression_parts_y.append(f'{var_name_pt} * sin({var_name_phi})')
+        args = make_RooArgList(variables)
+
+        name_px = self._name_partial_gen_htmiss_variable("px")
+        expression_x = '+'.join(expression_parts_x)
+        htmiss_px_variable = r.RooFormulaVar(
+            name_px,
+            expression_x,
+            args
+        )
+        self._wsimp(htmiss_px_variable)
+
+        name_py = self._name_partial_gen_htmiss_variable("py")
+        expression_y = '+'.join(expression_parts_y)
+        htmiss_py_variable = r.RooFormulaVar(
+            name_py,
+            expression_y,
+            args
+        )
+        self._wsimp(htmiss_py_variable)
+
+    def _build_gen_htmiss_xy_variable_from_px_py(self):
+        '''
+        Creates x and y variables for htmiss based on jet px, py inputs.
+        '''
         for direction in self._directions:
-            self._build_partial_gen_htmiss_variable(direction)
-        self._build_total_gen_htmiss_variable()
+            self._build_gen_htmiss_xy_variable_single_direction(direction)
+
+    def _build_gen_htmiss_xy_variable_single_direction(self, direction):
+        '''
+        Create gen HTmiss variable in x or y direction.
+        '''
+        expression_parts = []
+        variables = []
+        for index in range(self.njets):
+            var_name = self._name_gen_momentum_var(direction, index)
+            variables.append(self.ws.var(var_name))
+            expression_parts.append(var_name)
+        expression = '+'.join(expression_parts)
+
+        name = self._name_partial_gen_htmiss_variable(direction)
+        htmiss_partial_variable = r.RooFormulaVar(
+            name,
+            expression,
+            make_RooArgList(variables)
+        )
+
+    def _build_derived_gen_htmiss_pt_phi_variables(self):
+        '''
+        Create derived HTmiss pt, phi coordinates based on existing px, py.
+
+        This is just a basis transformation for convenience in future use.
+        '''
+        name_px = self._name_partial_gen_htmiss_variable("px")
+        htmiss_px_variable = self.ws.function(name_px)
+        name_py = self._name_partial_gen_htmiss_variable("py")
+        htmiss_py_variable = self.ws.function(name_py)
+        htmiss_pt_variable = r.RooFormulaVar(
+            self._name_partial_gen_htmiss_variable("pt"),
+            f'sqrt({name_px}**2 + {name_py}**2)',
+            r.RooArgList(htmiss_px_variable, htmiss_py_variable)
+        )
+        self._wsimp(htmiss_pt_variable)
+
+        # htmiss_phi_variable = r.RooFormulaVar(
+        #     self._name_partial_gen_htmiss_variable("phi"),
+        #     'arctan({name_py} / {name_px})',
+        #     r.RooArgList(htmiss_px_variable, htmiss_py_variable)
+        # )
+        # self._wsimp(htmiss_phi_variable)
+
+
 
     def _build_total_gen_htmiss_variable(self):
         partial_htmiss_variable_names = [self._name_partial_gen_htmiss_variable(direction) for direction in self._directions]
@@ -229,12 +314,12 @@ class RebalanceWSFactory(NamingMixin):
         slope_variable = r.RooRealVar(
             slope_name,
             slope_name,
-            -0.1,
+            -0.05,
         )
         self._wsimp(slope_variable)
 
         prior_pdf_name = self._name_total_gen_htmiss_prior_pdf()
-        htmiss_variable = self.ws.function(self._name_total_gen_htmiss_variable())
+        htmiss_variable = self.ws.function(self._name_partial_gen_htmiss_variable(direction='pt'))
         prior_pdf = r.RooExponential(
             prior_pdf_name,
             prior_pdf_name,
@@ -278,24 +363,12 @@ class RebalanceWSFactory(NamingMixin):
             indices = range(self.njets)
         return [naming_function(direction, index) for direction in directions for index in indices]
 
-    def _build_combined_momentum_pdf(self):
-        '''
-        Defines the product PDF of all individual jet PDFs.
-        '''
-        individual_pdf_names = self._expand_naming(self._name_jet_momentum_pdf)
-
-        individual_pdfs = [self.ws.function(name) for name in individual_pdf_names]
-
-        pdf_name = self._name_combined_momentum_pdf()
-        expression = '*'.join(individual_pdf_names)
-        combined_pdf = r.RooGenericPdf(
-                    pdf_name,
-                    pdf_name,
-                    expression,
-                    make_RooArgList(individual_pdfs)
-                    )
-
-        self._wsimp(combined_pdf)
+    def _variable_limits(self, direction, central_value):
+        if direction == 'phi':
+            return (None, None)
+        else:
+            lim = max(2*abs(central_value), 100)
+            return (-lim, lim)
 
     def _build_single_jet_momentum_vars(self, direction, index):
         '''
@@ -303,11 +376,17 @@ class RebalanceWSFactory(NamingMixin):
         '''
         jet = self.get_jet(index)
 
-        start = getattr(jet, direction)
-        lim = max(2*abs(start), 100)
+        central_value = getattr(jet, direction)
 
-        args = [start, -lim, lim]
+        args = [central_value]
 
+        # Limits depend on the direction,
+        # e.g. phi maybe assumed constant
+        # Only add limits to constructor if
+        # the variable is NOT constant
+        limits = self._variable_limits(direction, central_value)
+        if not any([x is None for x in limits]):
+            args.extend(limits)
 
         name_gen_var = self._name_gen_momentum_var(direction, index)
         gen_var = r.RooRealVar(
@@ -322,7 +401,7 @@ class RebalanceWSFactory(NamingMixin):
         reco_var = r.RooRealVar(
                                 name_reco_var,
                                 name_reco_var,
-                                start
+                                central_value
                                 )
         self._wsimp(reco_var)
 
@@ -333,34 +412,9 @@ class RebalanceWSFactory(NamingMixin):
         The jet resolution in a given direction for given jet index in GeV.
         '''
         jet = self.get_jet(index)
-        sigma = self._jer_evaluator.get_jer(jet.pt, jet.eta) * jet.pt
+        sigma = self._jer_evaluator.get_jer(jet.pt, jet.eta)
 
         return sigma * getattr(jet, direction)
-
-    def _build_single_jet_momentum_pdf(self, gen_var, reco_var, direction, index):
-        '''
-        Defines the PDF(reco | gen), i.e. the probability representing the agreement
-        between gen and reco momentum for a given direction and jet index.
-        '''
-        sigma = self._resolution(index, direction)
-        resolution_name = self._name_jet_resolution_var(direction, index)
-        resolution_var = r.RooRealVar(
-                                 resolution_name,
-                                 resolution_name,
-                                 sigma
-                                 )
-        self._wsimp(resolution_var)
-
-        pdf_name = self._name_jet_momentum_pdf(direction, index)
-        momentum_pdf = r.RooGaussian(
-                                    pdf_name,
-                                    pdf_name,
-                                    reco_var,
-                                    gen_var,
-                                    resolution_var
-                                    )
-
-        self._wsimp(momentum_pdf)
 
     def _build_single_jet(self, index):
         '''
@@ -369,3 +423,58 @@ class RebalanceWSFactory(NamingMixin):
         for direction in self._directions:
             gen_var, reco_var = self._build_single_jet_momentum_vars(direction, index)
             self._build_single_jet_momentum_pdf(gen_var, reco_var, direction, index)
+
+    def _build_single_jet_momentum_pdf(self, gen_var, reco_var, direction, index):
+        '''
+        Defines the PDF(reco | gen), i.e. the probability representing the agreement
+        between gen and reco momentum for a given direction and jet index.
+        '''
+
+        if gen_var.isConstant():
+            return
+        else:
+            pdf_name = self._name_jet_momentum_pdf(direction, index)
+            sigma = self._resolution(index, direction)
+            resolution_name = self._name_jet_resolution_var(direction, index)
+            resolution_var = r.RooRealVar(
+                                    resolution_name,
+                                    resolution_name,
+                                    sigma
+                                    )
+            self._wsimp(resolution_var)
+
+            momentum_pdf = r.RooGaussian(
+                                        pdf_name,
+                                        pdf_name,
+                                        reco_var,
+                                        gen_var,
+                                        resolution_var
+                                        )
+
+            self._wsimp(momentum_pdf)
+
+
+    def _build_combined_momentum_pdf(self):
+        '''
+        Defines the product PDF of all individual jet PDFs.
+        '''
+        individual_pdf_names = self._expand_naming(self._name_jet_momentum_pdf)
+
+        individual_pdfs, expression_parts = [], []
+        for name in individual_pdf_names:
+            func = self.ws.function(name)
+            if not func:
+                continue
+            individual_pdfs.append(func)
+            expression_parts.append(name)
+
+        pdf_name = self._name_combined_momentum_pdf()
+        expression = '*'.join(expression_parts)
+        combined_pdf = r.RooGenericPdf(
+                    pdf_name,
+                    pdf_name,
+                    expression,
+                    make_RooArgList(individual_pdfs)
+                    )
+
+        self._wsimp(combined_pdf)
