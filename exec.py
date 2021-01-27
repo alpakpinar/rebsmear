0a66b353a4bc7bb9713260f0abdd5c6f1c4aa4dc
Alp Akpinar <aakpinar@bu.edu>
2021-01-27 09:12:54 -0600

    Calculate and print RECO-HT value as well

--- exec.py
+++ exec.py
@@ -42,11 +42,12 @@ def extract_values_pt_phi(ws, tier):
     phi = np.array(phi)
     x = pt * np.cos(phi)
     y = pt * np.sin(phi)
-    htx = -np.sum(x)
-    hty = -np.sum(y)
+    htmiss_x = -np.sum(x)
+    htmiss_y = -np.sum(y)
 
+    ht = np.sum(pt)
 
-    return x, y, htx, hty
+    return x, y, htmiss_x, htmiss_y, ht
 
 def plot_plane(ws, tag):
     plt.gcf().clf()
@@ -54,8 +55,8 @@ def plot_plane(ws, tag):
     ax = plt.gca()
     
     # Reco
-    reco_x, reco_y, reco_htx, reco_hty = extract_values_pt_phi(ws, "reco")
-    gen_x, gen_y, gen_htx, gen_hty = extract_values_pt_phi(ws, "gen")
+    reco_x, reco_y, reco_htmissx, reco_htmissy, reco_ht = extract_values_pt_phi(ws, "reco")
+    gen_x, gen_y, gen_htmissx, gen_htmissy, gen_ht = extract_values_pt_phi(ws, "gen")
 
 
 
@@ -79,39 +80,46 @@ def plot_plane(ws, tag):
                 #  label='Gen jet' if i==0 else None
                  )
     
-    reco_ht = np.hypot(reco_htx, reco_hty)
-    gen_ht = np.hypot(gen_htx, gen_hty)
+    reco_htmiss = np.hypot(reco_htmissx, reco_htmissy)
+    gen_htmiss = np.hypot(gen_htmissx, gen_htmissy)
     arr_reco_ht = ax.arrow(
              x=0,
              y=0,
-             dx=reco_htx,
-             dy=reco_hty,
+             dx=reco_htmissx,
+             dy=reco_htmissy,
              head_width=10,
              color='navy',
              width=10,
              alpha=0.5,
             #  fill=False,
-             label=f'Reco $H_{{T}}^{{miss}}$ ({reco_ht:.0f} GeV)')
+             label=f'Reco $H_{{T}}^{{miss}}$ ({reco_htmiss:.0f} GeV)')
     arr_gen_ht = ax.arrow(
              x=0,
              y=0,
-             dx=gen_htx,
-             dy=gen_hty,
+             dx=gen_htmissx,
+             dy=gen_htmissy,
              head_width=10,
              color='crimson',
              width=10,
              alpha=0.5,
             #  fill=False,
-             label=f'Gen $H_{{T}}^{{miss}}$ ({gen_ht:.0f} GeV)')
+             label=f'Gen $H_{{T}}^{{miss}}$ ({gen_htmiss:.0f} GeV)')
 
-    axis_maximum = 1.2*max([abs(x) for x in (reco_x + reco_y + [reco_ht])])
+    axis_maximum = 1.2*max([abs(x) for x in (reco_x + reco_y + [reco_htmiss])])
     ax.set_ylim(-axis_maximum, axis_maximum)
     ax.set_xlim(-axis_maximum, axis_maximum)
     ax.set_title(f"{tag}, NLL = {ws.function('nll').getValV():.2f}")
-    ax.legend([arr_reco_jet, arr_gen_jet, arr_reco_ht, arr_gen_ht], ['RECO jet', 'GEN jet', f'RECO $H_{{T}}^{{miss}}$ ({reco_ht:.0f} GeV)', f'GEN $H_{{T}}^{{miss}}$ ({gen_ht:.0f} GeV)'])
+    ax.legend([arr_reco_jet, arr_gen_jet, arr_reco_ht, arr_gen_ht], ['RECO jet', 'GEN jet', f'RECO $H_{{T}}^{{miss}}$ ({reco_htmiss:.0f} GeV)', f'GEN $H_{{T}}^{{miss}}$ ({gen_htmiss:.0f} GeV)'])
 
     ax.set_xlabel(r'$p_x \ (GeV)$')
     ax.set_ylabel(r'$p_y \ (GeV)$')
+
+    ax.text(0.98, 0.1, f'$H_T^{{reco}} = {reco_ht:.2f}$ GeV',
+        fontsize=12,
+        ha='right',
+        va='top',
+        transform=ax.transAxes
+    )
 
     fig.savefig(f"output/test_{tag}.png", dpi=300)
 
