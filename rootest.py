ba35736b8c9dc565d446a76d7e42c77f00350272
Alp Akpinar <aakpinar@bu.edu>
2021-01-28 08:25:36 -0600

    Update test

--- rootest.py
+++ rootest.py
@@ -8,14 +8,17 @@ r.gROOT.SetBatch(True)
 
 pjoin = os.path.join
 
-# Dummy histogram
-th1 = r.TH1D("test","test",10,0,100)
-th1.Fill(5, 3)
-th1.Fill(15, 1)
-th1.Fill(25, 0.1)
+def main():
+    # Dummy histogram
+    th1 = r.TH1D("test","test",10,0,100)
+    th1.Fill(5, 3)
+    th1.Fill(15, 1)
+    th1.Fill(25, 0.1)
 
-# Elementary variables
-for px in [2,4,5,8]:
+    # Elementary variables
+    # for px in [2,4,5,8]:
+    # Test value for px
+    px=2
     px = r.RooRealVar("px","px",px,0,100)
 
     # Derived variables
@@ -24,7 +27,7 @@ for px in [2,4,5,8]:
     # Here comes the test:
     # Re-evaluate the value of pxsq as a RooRealVar, then feed into RooDataHist and RooHistPdf
     pxsq_var = r.RooRealVar('pxsqvar', 'pxsqvar', pxsq.evaluate())
-    print(pxsq_var)
+    # pxsq_var = r.RooRealVar('pxsqvar', 'pxsqvar', 10, 100)
 
     rdh = r.RooDataHist("test","test", r.RooArgList(pxsq_var), th1)
 
@@ -34,6 +37,18 @@ for px in [2,4,5,8]:
                     rdh
                 )
 
+    print(f'Value of px: {px}')
+    print(f'Value of pxsq: {pxsq_var}')
+    print(f'PDF: {pdf.getValV()}')
+
+    print('*'*20)
+    print('Setting px=5')
+    print('*'*20)
+    px.setVal(5)
+    print(f'Value of px: {px}')
+    print(f'Value of pxsq: {pxsq_var}')
+    print(f'PDF: {pdf.getValV()}')
+
     # Plot the PDF and save figure
     # frame = pxsq_var.frame(0,100)
     # pdf.plotOn(frame)
@@ -42,25 +57,21 @@ for px in [2,4,5,8]:
     # frame.Draw()
     # c.SaveAs('test.png')
 
-    print('Current PDF value:')
-    print(pdf.getValV())
+if __name__ == '__main__':
+    main()
 
 # Outputs:
-# =========================
-# Current PDF value:
-# 0.3
-# RooRealVar::pxsqvar = 16 C  L(-INF - +INF) 
+# =============================
+# Value of px: RooRealVar::px = 2  L(0 - 100) 
 
-# [#1] INFO:DataHandling -- RooDataHist::adjustBinning(test): fit range of variable pxsqvar expanded to nearest bin boundaries: [-1e+30,1e+30] --> [0,100]
-# Current PDF value:
-# 0.1
-# RooRealVar::pxsqvar = 25 C  L(-INF - +INF) 
+# Value of pxsq: RooRealVar::pxsqvar = 4 C  L(0 - 100) 
 
-# [#1] INFO:DataHandling -- RooDataHist::adjustBinning(test): fit range of variable pxsqvar expanded to nearest bin boundaries: [-1e+30,1e+30] --> [0,100]
-# Current PDF value:
-# 0.01
-# RooRealVar::pxsqvar = 64 C  L(-INF - +INF) 
+# PDF: 0.3
+# ********************
+# Setting px=5
+# ********************
+# Value of px: RooRealVar::px = 5  L(0 - 100) 
 
-# [#1] INFO:DataHandling -- RooDataHist::adjustBinning(test): fit range of variable pxsqvar expanded to nearest bin boundaries: [-1e+30,1e+30] --> [0,100]
-# Current PDF value:
-# 0.0
+# Value of pxsq: RooRealVar::pxsqvar = 4 C  L(0 - 100) 
+
+# PDF: 0.3
