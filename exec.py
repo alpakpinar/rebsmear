34116ef29af2ab19a6a8a0fd9e54023c773ef04d
Alp Akpinar <aakpinar@bu.edu>
2021-01-29 13:04:00 -0600

    Jet eta/phi argument fixup

--- exec.py
+++ exec.py
@@ -15,7 +15,7 @@ def read_jets(event):
     
     pt, phi, eta = (t[f'Jet_{x}'].array(entrystart=n, entrystop=n+1)[0] for x in ['pt','phi','eta'])
     
-    return [Jet(ipt, iphi, ieta) for ipt, iphi, ieta in zip(pt, phi, eta)]
+    return [Jet(pt=ipt, phi=iphi, eta=ieta) for ipt, iphi, ieta in zip(pt, phi, eta)]
 
 
 def extract_values(ws, tier):
