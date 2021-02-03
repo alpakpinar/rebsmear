1f2349eb71bbd1920f0a05000c4853fe4b7342c1
Alp Akpinar <aakpinar@bu.edu>
2021-02-03 11:52:15 -0500

    Plotting script to compare priors with GEN-HTmiss from QCD MC

--- plot/compare_with_gen_htmiss_qcd.py
+++ plot/compare_with_gen_htmiss_qcd.py
@@ -68,6 +68,60 @@ def compare_with_genhtmiss_dist(acc, distribution, jobtag, filetag, inputrootfil
     plt.close(fig)
     print(f'File saved: {outpath}')
 
+def comopare_prior_with_genhtmiss(acc, distribution, jobtag, filetag, logy=False):
+    '''Compare GEN-HTmiss distribution with the priors we use as input to rebalancing.'''
+    h = acc[distribution]
+    # Get all the events
+    h = h.integrate('dataset').integrate('region', 'inclusive')
+
+    # Plot the GEN HT-miss distribution from coffea 
+    fig, ax = plt.subplots()
+    # Normalize
+    total_sumw = np.sum(h.values()[()])
+    h.scale(1/total_sumw)
+    hist.plot1d(h, ax=ax, binwnorm=1)
+
+    # Read and plot the priors from the input file
+    inputpriorfile = uproot.open('../input/htmiss_prior.root')
+    ht_bins_for_prior = [
+        '700_to_900',
+        '900_to_1300',
+    ]
+
+    def label_for_htbin(htbin):
+        lo, hi = re.findall('\d+', htbin)
+        return f'Prior: {lo} < $H_T$ < {hi}'
+
+    for htbin in ht_bins_for_prior:
+        ph = inputpriorfile[f'gen_htmiss_ht_{htbin}']
+        binw = np.diff(ph.edges)
+        hep.histplot(ph.values / binw, ph.edges, ax=ax, label=label_for_htbin(htbin))
+
+    ax.set_xlim(0,500)
+    if logy:
+        ax.set_yscale('log')
+        ax.set_ylim(1e-8,1e0)
+
+    handles, labels = ax.get_legend_handles_labels()
+    for handle, label in zip(handles, labels):
+        if label == 'None':
+            handle.set_label('GEN $H_T^{miss}$ from QCD MC \n ($700 < H_T < 1000 \ GeV$)')
+
+    ax.legend(handles=handles)
+    ax.set_ylabel('Normalized Yields')
+
+    # Save figure
+    outdir = f'./output/{jobtag}'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    if logy:
+        outpath = pjoin(outdir, f'prior_htmiss_comp_logy.pdf')
+    else:
+        outpath = pjoin(outdir, f'prior_htmiss_comp.pdf')
+    fig.savefig(outpath)
+    plt.close(fig)
+    print(f'File saved: {outpath}')
+
 def main():
     # Input ROOT file
     inputrootpath = sys.argv[1]
@@ -85,6 +139,10 @@ def main():
 
     compare_with_genhtmiss_dist(acc, distribution, jobtag, filetag, inputrootfile)
     compare_with_genhtmiss_dist(acc, distribution, jobtag, filetag, inputrootfile, logy=True)
+
+    # Compare priors with GEN-level HTmiss we obtain
+    comopare_prior_with_genhtmiss(acc, distribution, jobtag, filetag)
+    comopare_prior_with_genhtmiss(acc, distribution, jobtag, filetag, logy=True)
 
 if __name__ == '__main__':
     main()
