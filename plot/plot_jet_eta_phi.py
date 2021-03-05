dd5d1f3681a889b2bd3e7dde1ab37d0929220a6d
Alp Akpinar <aakpinar@bu.edu>
2021-03-05 15:49:09 -0500

    Update jet eta/phi plotting accordingly

--- plot/plot_jet_eta_phi.py
+++ plot/plot_jet_eta_phi.py
@@ -24,21 +24,20 @@ def get_xlabel(distribution):
 
     return mapping[distribution]
 
-def plot_jet_eta_phi(f, outtag, distribution):
-    h_lo = f[f'{distribution}_lowdhtmiss']
-    h_hi = f[f'{distribution}_highdhtmiss']
+def plot_jet_eta_phi(f, outtag, distribution, htmiss_thresh=100):
+    h_lo = f[f'{distribution}_low_final_htmiss']
+    h_hi = f[f'{distribution}_high_final_htmiss']
 
     fig, ax = plt.subplots()
-    hep.histplot(h_lo.values, h_lo.edges, ax=ax, label=r'$\Delta H_T^{miss} < 80 \ GeV$')
-    hep.histplot(h_hi.values, h_hi.edges, ax=ax, label=r'$\Delta H_T^{miss} > 80 \ GeV$')
+    hep.histplot(h_lo.values, h_lo.edges, ax=ax, label=f'$H_T^{{miss}} < {htmiss_thresh} \ GeV$ (final)')
+    hep.histplot(h_hi.values, h_hi.edges, ax=ax, label=f'$H_T^{{miss}} > {htmiss_thresh} \ GeV$ (final)')
 
     ax.set_yscale('log')
     ax.set_ylim(1e-2,1e4)
     ax.set_xlabel(get_xlabel(distribution))
     ax.legend()
 
-    htmiss_bef_thresh=120
-    ax.text(0., 1., f'$H_T^{{miss}} > {htmiss_bef_thresh}$ GeV (before reb.)',
+    ax.text(0., 1., f'$H_T^{{miss}} > {htmiss_thresh}$ GeV (before reb.)',
         fontsize=14,
         ha='left',
         va='bottom',
@@ -54,7 +53,7 @@ def plot_jet_eta_phi(f, outtag, distribution):
 
     print(f'File saved: {outpath}')
 
-def plot_jet_eta_phi_2d(f, outtag, distribution):
+def plot_jet_eta_phi_2d(f, outtag, distribution, htmiss_thresh=100):
     h = f[distribution]
 
     fig, ax = plt.subplots()
@@ -77,9 +76,9 @@ def plot_jet_eta_phi_2d(f, outtag, distribution):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
 
-    delta_htmiss_thresh=80
-    sgn = '<' if 'lowdhtmiss' in distribution else '>'
-    ax.text(0., 1., f'$\Delta H_T^{{miss}} {sgn} {delta_htmiss_thresh}$ GeV',
+    
+    sgn = '<' if 'low_final_htmiss' in distribution else '>'
+    ax.text(0., 1., f'$H_T^{{miss}} {sgn} {htmiss_thresh}$ GeV (after reb.)',
         fontsize=14,
         ha='left',
         va='bottom',
@@ -108,12 +107,12 @@ def main():
 
     # 2D eta vs. phi distributions
     distributions_2d = [
-        'ak4_eta_phi_lowdhtmiss',
-        'ak4_eta_phi_highdhtmiss',
-        'ak4_eta_phi0_lowdhtmiss',
-        'ak4_eta_phi0_highdhtmiss',
-        'ak4_eta_phi1_lowdhtmiss',
-        'ak4_eta_phi1_highdhtmiss',
+        'ak4_eta_phi_low_final_htmiss',
+        'ak4_eta_phi_high_final_htmiss',
+        'ak4_eta_phi0_low_final_htmiss',
+        'ak4_eta_phi0_high_final_htmiss',
+        'ak4_eta_phi1_low_final_htmiss',
+        'ak4_eta_phi1_high_final_htmiss',
     ]
     for distribution in distributions_2d:
         plot_jet_eta_phi_2d(f, outtag, distribution=distribution)
